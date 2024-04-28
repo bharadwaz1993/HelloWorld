7cea2b674cf2d153ab34c2c5be8561274b008bc5
bharadwazuppala <bharadwaz.uppala@gmail.com>
2024-04-29 02:07:19 +0530

    configured project id and repo address in main.yaml

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res) => {
-    res.send('Hello my Name is Bharadwaz and this is a GCP Demo Project')
+    res.send('Hello my Name is Bharadwaz Uppala and this is a GCP Demo Project')
 })
 
 app.listen(3000)
